8e14822a78c7578d0bbd8d310697a8d725241343
Zeeppss <69776277+Zeeppss@users.noreply.github.com>
2020-10-07 20:46:08 +0700

    Create 4-DataTypes.kt

---
+++ 4-DataTypes.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    var nama: String
+    nama = "Zeeppss"
+
+    var umur: Int = 10
+    var umurku = 10
+
+    var hidup: Boolean = true 
+    var tanda: Float = 97.4F
+    var persentase: Double = 90.78
+    var kelamin: Char = 'M'
+
+    print(tanda)
+}
