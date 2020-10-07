90d68b9ec9cf941b568192a00ab08181d3fef6dc
Zeeppss <69776277+Zeeppss@users.noreply.github.com>
2020-10-07 20:40:52 +0700

    Create Comments.kt

---
+++ Comments.kt
@@ -0,0 +1,11 @@
+/*
+* Ini adalah komentar baris 1
+*
+* Ini adalah komentar baris 2
+*
+* Ini adalah fungsi utama. Titik masuk aplikasi.
+* */
+
+fun main(args: Array<String>) {     // Ini adalah komentar sebaris...
+    print("Hello World")
+}
